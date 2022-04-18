f8ebe4c09400411c7ad5788a211fbad0e9871c9d
tautvydas.javaisis <tautvydas.javaisis@nfq.lt>
2022-04-18 21:43:57 +0300

    make it better

--- src/main/java/com/tietoevry/backend/authorization/WebConfig.java
+++ src/main/java/com/tietoevry/backend/authorization/WebConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
             .allowedMethods("PUT", "DELETE")
             .allowedHeaders("header1", "header2", "header3")
             .exposedHeaders("header1", "header2")
